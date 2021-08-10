397e9fe852f2ca2a8744b0c4f565a15c77ec713b
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-08-10 15:54:50 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Button from 'sveltestrap/src/Button.svelte';
+  import Popover from 'sveltestrap/src/Popover.svelte';
+
+  const placements = ['top', 'right', 'left', 'bottom'];
+</script>
+
+{#each placements as placement}
+  <div class="mt-3">
+    <Button id={`btn-${placement}`}>Click for {placement}</Button>
+    <Popover
+      target={`btn-${placement}`}
+      {placement}
+      title={`Popover ${placement}`}
+    >
+      This is a Popover on the {placement} of the trigger.
+    </Popover>
+  </div>
+{/each}
---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import node from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+        kit: {
+          adapter: node({
+            out: 'build',
+          }),
+          // hydrate the <div id="svelte"> element in src/app.html
+          target: '#svelte'
+        }
+};
+
+export default config;
